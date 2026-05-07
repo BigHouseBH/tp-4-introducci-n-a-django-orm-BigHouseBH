ff7259607add670c82c3de670cad0f74eeb7dccf
BigHouseBH <[TU-EMAIL@gmail.com]>
2026-05-06 23:27:50 -0300

    feat: implementar consultas ORM en queries.py

--- catalogo/queries.py
+++ catalogo/queries.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F
 
 from .models import Autor, Libro
 
+#Mientras en models se define "como guardar" en quearies(consulta) se define "como retirar"
 
 def libros_por_categoria(nombre_categoria: str):
     """
@@ -23,9 +24,10 @@ def libros_por_categoria(nombre_categoria: str):
     # TODO: implementar la consulta ORM
     # Pista: usá filter con la relación M2M
     #   Libro.objects.filter(categorias__nombre=nombre_categoria)
-    raise NotImplementedError
-
-
+    return Libro.objects.filter(categorias__nombre=nombre_categoria)
+    
+    #[nombre de modelo].objets: "dame todos los objetos de este modelo"
+    
 def autores_con_mas_de_n_libros(n: int):
     """
     Devuelve un QuerySet de Autores que tienen más de n libros en el catálogo.
@@ -45,7 +47,11 @@ def autores_con_mas_de_n_libros(n: int):
     #   Autor.objects.annotate(cantidad_libros=Count("libro"))
     # Pista 2: luego filtrá
     #   .filter(cantidad_libros__gt=n)
-    raise NotImplementedError
+    return Autor.objects.annotate(cantidad_libros=Count("libro")).filter(cantidad_libros__gt=n)
+    
+    #annotate:crea una hoja de calculo temporal(no se almacena en la BD) y almaneca en cada fila el atributo en parametro
+    #Count("libro") cuenta la cantidad de libros relacionados a cada autor
+    #luego filtra de esta ""notas""(la hoja de calculo temporal)
 
 
 def libros_sin_disponibilidad():
@@ -65,9 +71,13 @@ def libros_sin_disponibilidad():
         ).filter(activos=models.F("cantidad_total"))
     """
     # TODO: implementar con annotate + F expression + filter
-    raise NotImplementedError
+    return Libro.objects.annotate(
+            activos=Count("prestamo", filter=Q(prestamo__fecha_devolucion__isnull=True))
+        ).filter(activos=F("cantidad_total"))
 
-
+    #Q es una clase que permite construir consultas complejas con operadores lógicos (AND, OR, NOT)
+    
+    
 def top_n_libros_mas_prestados(n: int):
     """
     Devuelve los N libros con más préstamos (en total, sin importar si están activos).
@@ -80,7 +90,7 @@ def top_n_libros_mas_prestados(n: int):
 
     Pista:
         Libro.objects.annotate(total_prestamos=Count("prestamo"))
-                     .order_by("-total_prestamos")[:n]
+                    .order_by("-total_prestamos")[:n]
     """
     # TODO: implementar con annotate + order_by + slicing
-    raise NotImplementedError
+    return Libro.objects.annotate(total_prestamos=Count("prestamo")).order_by("-total_prestamos")[:n]
